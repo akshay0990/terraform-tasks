5053ef3de4ce0d27ac76b38d52eb6790a037e646
akshay0990 <84656396+akshay0990@users.noreply.github.com>
2021-07-08 07:44:15 +0530

    Add files via upload

--- main.tf
+++ main.tf
@@ -1,17 +1,28 @@
 # Configure the AWS Provider
 provider "aws" {
   region = "us-east-1"
-  access_key  = "AKIAZORCJCBVGRUCN326"
-  secret_key  = "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"
+  access_key  = ""
+  secret_key  = ""
 }
 
-resource "aws_iam_user" "terraform_user" {
-  name = "terraform_user"
+resource "aws_iam_user" "terraform_user1" {
+  name = "terraform_user1"
   path = "/"
   force_destroy = true
 }
 
-resource "aws_iam_user_login_profile" "terraform_user" {
-  user    = aws_iam_user.terraform_user.name
+resource "aws_iam_user" "terraform_user2" {
+  name = "terraform_user2"
+  path = "/"
+  force_destroy = true
+}
+
+resource "aws_iam_user_login_profile" "terraform_user1" {
+  user    = aws_iam_user.terraform_user1.name
+  pgp_key = "keybase:test"
+}
+
+resource "aws_iam_user_login_profile" "terraform_user2" {
+  user    = aws_iam_user.terraform_user2.name
   pgp_key = "keybase:test"
 }
